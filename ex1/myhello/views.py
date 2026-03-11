6da6a2da89dd21d45a8cae4c2f39d37899b3b08f
你的名字 < Email>
2026-03-11 16:21:52 +0800

    Final: Ex1 directory ready with Django environment confirmed

--- ex1/myhello/views.py
+++ ex1/myhello/views.py
@@ -1,16 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import logging
-
-logger = logging.getLogger('django')
 
 @api_view(['GET'])
 def myhello_api(request):
     my_name = request.GET.get('name', None)
     
-    logger.debug(f"************** myhello_api: {my_name}")
-
     if my_name:
         return Response({"data": "Hello " + my_name}, status=status.HTTP_200_OK)
     else:
